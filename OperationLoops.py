f7466124050ff9a25235736a703683e0b958b222
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-05-25 21:58:10 +0800

    Added Import or Call function

--- OperationLoops.py
+++ OperationLoops.py
@@ -1,3 +1,5 @@
 #Import or call the main calculator file
+from CallenculatorV2 import *
+
 #Create a class for looping which operations to use
 #Create another class to ask the user if they want to try the calculator again.
