cab5343ab03050a15ab970d6a0c9e4b9c8734a51
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-05-25 15:39:21 +0800

    Pseudocodes

---
+++ CallenculatorV2.py
@@ -0,0 +1,7 @@
+#Create a class for the calculator
+#Use the init function to set default values for variables and a welcome mesage
+#Create a function for addition
+#Create a function for subtraction
+#Create a function for multiplication
+#Create a function for division            
+#Create a variable for the class 
