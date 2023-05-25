62e1e8cd7493d2de79dde960e81aad050312e1e6
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-05-25 21:25:41 +0800

    Added a function for Subtraction

--- CallenculatorV2.py
+++ CallenculatorV2.py
@@ -16,7 +16,17 @@ class Callenculator:
             print("Sum:", self.result)
         except ValueError:
             print("Error: Looks like someone doesn't know their ABCs and 123s (Please enter numbers only).")
-#Create a function for subtraction
+    
+    #Create a function for subtraction
+    def subtract(self):
+        try:
+            self.num1 = float(input("Enter the first number: "))
+            self.num2 = float(input("Enter the second number: "))
+            self.result = self.num1 - self.num2
+            print("Difference:", self.result)
+        except ValueError:
+            print("Error: Looks like someone doesn't know their ABCs and 123s (Please enter numbers only).")
+            
 #Create a function for multiplication
 #Create a function for division            
 #Create a variable for the class 
