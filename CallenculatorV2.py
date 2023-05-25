d5f79dfce972d612387db351416faf72f1b05e6e
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-05-25 21:17:44 +0800

    Added function for addition.

--- CallenculatorV2.py
+++ CallenculatorV2.py
@@ -7,7 +7,15 @@ class Callenculator:
         self.result = 0
         print("Welcome to Callenculator! The sassiest calculator you can find.")
         
-#Create a function for addition
+    #Create a function for addition
+    def add(self):
+        try:
+            self.num1 = float(input("Enter the first number: "))
+            self.num2 = float(input("Enter the second number: "))
+            self.result = self.num1 + self.num2
+            print("Sum:", self.result)
+        except ValueError:
+            print("Error: Looks like someone doesn't know their ABCs and 123s (Please enter numbers only).")
 #Create a function for subtraction
 #Create a function for multiplication
 #Create a function for division            
