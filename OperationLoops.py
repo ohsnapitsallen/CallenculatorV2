77c69c95d80ada809e37877aabfd0f3ddb499e4e
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-06-04 15:38:31 +0800

    Edited functionality of program for inheritance to work

--- OperationLoops.py
+++ OperationLoops.py
@@ -3,7 +3,7 @@ from CallenculatorV2 import *
 
 #Create a class for looping which operations to use
 class loop:
-    def __init__(self):
+    def __init__(self, operate = ' '):
         self.choice = ' '
         while True:
             print("Please select what kind of calculator you would want to use")
@@ -13,26 +13,45 @@ class loop:
             print("4. Division")
 
             self.choice = input("Enter your choice (1-4): ")
+            try:
+                num1 = float(input("Enter the first number: "))
+                num2 = float(input("Enter the second number: "))
 
-            #Make an if-statement which calls all the functions from the main calculator file depending on the user input
-            if self.choice == '1':
-                    callenculator.add()
+                #Make an if-statement which calls all the functions from the main calculator file depending on the user input
+                if self.choice == '1':
+                    operate = add()
+                    operate.solve(num1, num2)
+                    print("Sum:", operate.result)
                     break
 
-            elif self.choice == '2':
-                    callenculator.subtract()
+                elif self.choice == '2':
+                    operate = subtract()
+                    operate.solve(num1, num2)
+                    print("Difference:", operate.result)
                     break
 
-            elif self.choice == '3':
-                    callenculator.multiply()
+                elif self.choice == '3':
+                    operate = multiply()
+                    operate.solve(num1, num2)
+                    print("Product:", operate.result)
                     break
 
-            elif self.choice == '4':
-                    callenculator.divide()
+                elif self.choice == '4':
+                    operate = divide()
+                    operate.solve(num1, num2)
+                    print("Quotient:", operate.result)
                     break
 
-            else:
-                print("Numbers ain't that hard to learn buddy. (Enter only numbers from 1 to 4)")
+                else:
+                    print("Numbers ain't that hard to learn buddy. (Enter only numbers from 1 to 4)")
+                    
+            except ValueError:
+                print("Error: Looks like someone doesn't know their ABCs and 123s (Please enter numbers only).")
+                
+            except ZeroDivisionError:
+                print("Error: Have you tried dividing 0 cookies to your 0 friends (You cannot devide by zero)")
+
+                
 
                 
 #Create another class to ask the user if they want to try the calculator again.
@@ -45,6 +64,7 @@ class question:
             if self.response == "y":
                 loop()
             elif self.response == "n":
+                print("Thank You")
                 break
             else:
                 print("Hey! that's not 'y' nor 'n'. (Please Try Again)")
